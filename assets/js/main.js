ac4596b46910f9999cb219057947215e8b8ae306
Ashraful Ferdous <ashraful@boo2.nl>
2023-07-22 11:04:59 +0600

    newletter form completed.

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,11 @@ document.addEventListener("DOMContentLoaded", function() {
     emailaddressInput = document.getElementById("emailAddress");
     labelconEls = document.getElementsByClassName("label-container");
     document.getElementById("btnSubmit").addEventListener("click", onSubmitFunc);
+    document.getElementById("btnDismiss").addEventListener("click", function() {
+
+        showHideForm('show');
+        emailaddressInput.value = "";
+    });
     emailaddressInput.addEventListener("keyup", validateWhileType);
 });
 
@@ -35,6 +40,11 @@ function onSubmitFunc(e)
         }
 
         emailaddressInput.classList.add("error");
+    } else {
+
+        showHideForm("hide");
+        
+        document.getElementById("messageEmailHolder").innerHTML = email.trim();
     }
 }
 
@@ -63,6 +73,35 @@ function validateWhileType(e)
             emailaddressInput.classList.remove("error");
         }
     }, 500);
+}
+
+function showHideForm(operation)
+{
+    if (operation==="hide") {
+
+        var successMessageContainers = document.getElementsByClassName("success-message-container");
+        var newsletterContainer = document.getElementsByClassName("newsletter-container");
+        for (var counter = 0; counter < successMessageContainers.length; counter++) {
+
+            successMessageContainers[counter].removeAttribute("style");
+            for (var counter1 = 0; counter1 < newsletterContainer.length; counter1++) {
+
+                newsletterContainer[counter1].style.display = "none";
+            }
+        }
+    } else {
+
+        var successMessageContainers = document.getElementsByClassName("success-message-container");
+        var newsletterContainer = document.getElementsByClassName("newsletter-container");
+        for (var counter1 = 0; counter1 < newsletterContainer.length; counter1++) {
+
+            newsletterContainer[counter1].removeAttribute("style");
+            for (var counter = 0; counter < successMessageContainers.length; counter++) {
+
+                successMessageContainers[counter].style.display = "none";
+            }
+        }
+    }
 }
 
 function validateEmail (email) {
